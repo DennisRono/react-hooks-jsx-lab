864aa5bdb646fe55c5a9deca62be17ed2a70147c
DennisRono <dennisrkibet@gmail.com>
2024-07-29 14:55:31 +0300

    react day one Lab

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { image } from '../data/data'
+
+function About() {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam vel
+        officia, voluptatibus dolorem laboriosam eum?
+      </p>
+      <img src={image} alt="I made this" />
+    </div>
+  )
+}
+
+export default About
